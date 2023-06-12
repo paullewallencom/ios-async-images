ab88a36284d5991cb8424df2fd0cd6d91b5c4952
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-11 18:58:48 -0700

    Updated Files
    Updated files.

--- FatImages/ViewController.swift
+++ FatImages/ViewController.swift
@@ -55,14 +55,60 @@ class ViewController: UIViewController {
     
     // this method avoids blocking by creating a background queue, without blocking the UI
     @IBAction func simpleAsynchronousDownload(sender: UIBarButtonItem) {
-        // TODO: finish implementation
-        print("TODO: simpleAsynchronousDownload")
+        // start animation
+        activityView.startAnimating()
+        
+        // create a queue
+        let downloadQueue = DispatchQueue(label: "download", qos: .userInitiated)
+        
+        // run the blocking operation in the background
+        downloadQueue.async {
+            // use url to get the data for the image
+            if let url = URL(string: BigImages.shark.rawValue), let imgData = try? Data(contentsOf: url) {
+                // turn data into an image
+                let image = UIImage(data: imgData)
+                
+                // run  code that updates the UI on the main queue
+                DispatchQueue.main.async {
+                    // display it
+                    self.photoView.image = image
+                    
+                    // stop animating
+                    self.activityView.stopAnimating()
+                }
+            }
+        }
     }
     
     // MARK: - Async Download (with Completion Handler)
     
     @IBAction func asynchronousDownload(sender: UIBarButtonItem) {
-        // TODO: finish implementation
-        print("TODO: asynchronousDownload")
+        // start animation
+        activityView.startAnimating()
+        
+        downloadBigImage { (image) -> Void in
+            // display it
+            self.photoView.image = image
+            
+            // stop animating
+            self.activityView.stopAnimating()
+        }
+    }
+    
+    // this method downloads a huge image on a global queue
+    // once finished, the completion closure runs with the image
+    func downloadBigImage(completionHandler handler: @escaping (UIImage?) -> Void){
+        DispatchQueue.global(qos: .userInitiated).async {
+            // use url to get the data for the image
+            if let url = URL(string: BigImages.whale.rawValue), let imgData = try? Data(contentsOf: url) {
+                // turn data into an image
+                let image = UIImage(data: imgData)
+                
+                // always run completion handler in the main queue, just in case!
+                DispatchQueue.main.async {
+                    handler(image)
+                }
+            }
+        }
     }
 }
